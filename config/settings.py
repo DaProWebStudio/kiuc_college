10f7428270b8021c8fcc8e579c2449a1dd24af93
WayupKG <adikgk232323@gmail.com>
2023-04-25 00:23:01 +0600

    perf: added imagekit and django_cleanup

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django_cleanup',
+    'imagekit',
     'ckeditor',
     'ckeditor_uploader',
 

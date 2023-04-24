e8e75021dd7cc99a46c27512daf5999c57ce939d
WayupKG <adikgk232323@gmail.com>
2023-04-25 01:31:22 +0600

    feat: added main.js

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+// скролл топ
+const scrollBtn = document.querySelector('.upShow');
+
+window.onscroll = () => {
+    if (window.scrollY > 500) {
+        scrollBtn.classList.remove('upShow__hide');
+    } else if (window.scrollY < 500) {
+        scrollBtn.classList.add('upShow__hide');
+    }
+};
+
+scrollBtn.onclick = () => {
+    window.scrollTo(0, 0)
+}

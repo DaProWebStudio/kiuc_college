759b7ab1e3556976ef8a12c337a2b768083fc27c
WayupKG <adikgk232323@gmail.com>
2023-04-25 21:47:22 +0600

    fix: changed get name
    fix: added urls for specialty

--- common/upload_to_files.py
+++ common/upload_to_files.py
@@ -8,19 +8,20 @@ def specialty_main_img(instance, filename):
 
 def news_main_img(instance, filename):
     list_file = filename.split('.')
-    return f'News/{instance.slug[0:35]}/{get_slug(list_file[0])}.{list_file[-1]}/'
+    title = instance.slug[0:35]
+    return f'news/{title}/{title}.{list_file[-1]}'
 
 
 def news_news_img(instance, filename):
     list_file = filename.split('.')
-    return f'News/{instance.news.slug[0:35]}/{get_slug(list_file[0])}.{list_file[-1]}/'
+    return f'news/{instance.news.slug[0:35]}/{instance.news.slug[0:35]}.{list_file[-1]}'
 
 
 def upload_to_file(instance, filename):
     list_file = filename.split('.')
-    return f'teams/{get_slug(instance.position.name)}/{get_slug(instance.slug)}.{list_file[-1]}/'
+    return f'teams/{get_slug(instance.position.name)}/{get_slug(instance.slug)}.{list_file[-1]}'
 
 
 def upload_to_recaptcha(instance, filename):
     list_file = filename.split('.')
-    return f'reCaptcha/{get_slug(list_file[0])}.{list_file[-1]}/'
+    return f'reCaptcha/{get_slug(list_file[0])}.{list_file[-1]}'

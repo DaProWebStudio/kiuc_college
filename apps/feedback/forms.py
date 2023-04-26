941278091de0aab54569d9d353fe1abb865b9424
WayupKG <adikgk232323@gmail.com>
2023-04-27 00:15:17 +0600

    fix: added recaptcha for feedback

--- apps/feedback/forms.py
+++ apps/feedback/forms.py
@@ -1,31 +1,35 @@
-from django.forms import ModelForm, Textarea, TextInput, EmailInput
+from django.forms import ModelForm, Textarea, TextInput, EmailInput, IntegerField, NumberInput
 from django.utils.translation import gettext_lazy as _
 
 from .models import FeedBack
 
 
 class CreateFeedBackForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['required'] = 'required'
+
+    user_answer = IntegerField(widget=NumberInput(attrs={'class': 'input', 'placeholder': _('Напишите ответ')}))
+
     class Meta:
         model = FeedBack
-        fields = ['name', 'email', 'message']
+        fields = ['name', 'email', 'message', 'user_answer']
         widgets = {
             'name': TextInput(attrs={
                 'class': 'input',
                 "placeholder": _("Ваше имя"),
                 "minlength": "3",
                 "maxlength": "25",
-                "required": "required"
             }),
             'email': EmailInput(attrs={
                 'class': 'input',
                 "placeholder": _("Ваша электронная почта"),
-                "required": "required"
             }),
             'message': Textarea(attrs={
                 'class': 'message',
                 "placeholder": _("Ваше сообщение"),
                 "minlength": "20",
                 "maxlength": "5000",
-                "required": "required"
             }),
         }

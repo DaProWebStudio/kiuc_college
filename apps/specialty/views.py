2a24677042c1c367a704021e418550913d8c2951
WayupKG <adikgk232323@gmail.com>
2023-05-01 00:30:13 +0600

    fix: changed path template for specialty

--- apps/specialty/views.py
+++ apps/specialty/views.py
@@ -1,18 +1,16 @@
 from django.views.generic import TemplateView, FormView, ListView, DetailView
 from django.utils.translation import gettext_lazy as _
 
-from apps.feedback.forms import CreateFeedBackForm
-from apps.feedback.models import Recaptcha
 from apps.specialty.models import Specialty
 
 
 class SpecialtyListView(ListView):
     model = Specialty
     context_object_name = 'specialties'
-    template_name = 'list.html'
+    template_name = 'specialty/list.html'
 
 
 class SpecialtyDetailView(DetailView):
     model = Specialty
     context_object_name = 'specialty'
-    template_name = 'detail.html'
+    template_name = 'specialty/detail.html'

3ee7e5e3cc3c268c93c5f7d6482cf6af2403e6a0
WayupKG <adikgk232323@gmail.com>
2023-06-06 11:09:04 +0600

    feat: added page employee list and employee detail and style

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Employee, Position
+
+
+class EmployeeListView(ListView):
+    model = Employee
+    queryset = model.active.select_related('position')
+    context_object_name = 'employees'
+    template_name = 'employee/index.html'
+
+
+class EmployeeDetailView(DetailView):
+    model = Employee
+    queryset = model.active.select_related('position')
+    slug_field = 'slug'
+    context_object_name = 'employee'
+    template_name = 'employee/employee_detail.html'

66fe98898906f398da29903d1377aab705203d6f
WayupKG <adikgk232323@gmail.com>
2023-06-06 12:13:35 +0600

    feat: added page StudentLive

--- apps/student/views.py
+++ apps/student/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.utils.translation import gettext_lazy as _
 
-from .models import StudentCouncil
+from .models import StudentCouncil, StudentLive, StudentLiveImages
 
 
 class StudentCouncilView(ListView):
@@ -17,3 +19,30 @@ class StudentDetailView(DetailView):
     slug_field = 'slug'
     context_object_name = 'student'
     template_name = 'student/student_detail.html'
+
+
+class StudentLiveListView(ListView):
+    model = StudentLive
+    queryset = model.active.all()
+    context_object_name = 'news'
+    template_name = 'news/list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = _('Студенческая жизнь')
+        context['sub_title'] = _('Студентам')
+        return context
+
+
+class StudentLiveDetailView(DetailView):
+    model = StudentLive
+    queryset = model.active.prefetch_related(
+        Prefetch('images', StudentLiveImages.objects.only('image'))
+    )
+    context_object_name = 'item'
+    template_name = 'news/detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Студенческая жизнь')
+        return context
